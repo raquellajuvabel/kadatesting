d365f7108486c1c3dc66f1858cf12039f9bba31a
raquellajuvabel <raquellajuvabel29@gmail.com>
2026-02-27 14:16:30 +0700

    index tanpa html

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors());
 ====================== */
 
 app.get("/", (req, res) => {
-  res.send('<h1 style="color: pink;">Hello Raquella!</h1>');
+  res.send('Hello Raquella!');
 });
 
 app.get("/say/:greeting", (req, res) => {
