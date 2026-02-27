2d6a7ddead1f951b1c30a2abc51b56fc4195c097
raquellajuvabel <raquellajuvabel29@gmail.com>
2026-02-27 11:03:06 +0700

    penambahan srv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import noteRouter from "./routes/notes.js";
 
 const app = express();
 const uri =
-  "mongodb://raquellaraquellaa_db_user:admin123@ac-ubalf4g-shard-00-00.p8srec1.mongodb.net:27017,ac-ubalf4g-shard-00-01.p8srec1.mongodb.net:27017,ac-ubalf4g-shard-00-02.p8srec1.mongodb.net:27017/test?ssl=true&replicaSet=atlas-3z36w4-shard-0&authSource=admin";
+  "mongodb+srv://raquellaraquellaa_db_user:admin123@ac-ubalf4g-shard-00-00.p8srec1.mongodb.net:27017,ac-ubalf4g-shard-00-01.p8srec1.mongodb.net:27017,ac-ubalf4g-shard-00-02.p8srec1.mongodb.net:27017/test?ssl=true&replicaSet=atlas-3z36w4-shard-0&authSource=admin";
 
 mongoose
   .connect(uri, { serverSelectionTimeoutMS: 5000 })
